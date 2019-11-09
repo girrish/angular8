c50bbde6d9795b992a5a190b45f7615b90ab82ce
Girish Pawar <girish.pawar@icloud.com>
2019-11-09 17:57:19 +0530

    property Binding Servers Component

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   //template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>
+  //  <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
 
   ngOnInit() {
   }
