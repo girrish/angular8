84c3a3736dffc6449b66040ea3f4b6f46c9670d1
Girish Pawar <girish.pawar@icloud.com>
2019-11-27 05:16:35 +0530

    ng For and IF

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  // numbers = [1, 2, 3, 4, 5, 6, 7];
+  evenNumbers = [2, 4, 6, 8];
+  oddNumbers = [1, 3, 5, 7];
+  onlyOdd = false;
 }
