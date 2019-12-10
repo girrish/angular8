7ce477c35d401a7532dfc37e22e21485eecddd23
Girish Pawar <girish.pawar@icloud.com>
2019-12-10 23:39:53 +0530

    all about account service
    Injectable..
    emitting events from service
    catch emitted event from service in any component using subcriber

--- my-first-app/src/app/new-account/new-account.component.ts
+++ my-first-app/src/app/new-account/new-account.component.ts
@@ -1,27 +1,38 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+
+import { AccountsService } from './../accounts-service';
 import { LoggingService } from './../logging.service';
+
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountsService.addAccount(accountName, accountStatus);
+
     // console.log('A server status changed, new status: ' + accountStatus);
     // const logService = new LoggingService();
     // logService.logStatusChange(accountStatus);
 
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
 
   }
 }
